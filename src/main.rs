98f92d7b9253bad0163b7869b59fad849fb035ab
Daniel Werner <dan@casperlabs.io>
2021-08-30 10:47:09 -0700

    make tool generic

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,24 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opts {
-    #[structopt(short = "p", name = "Path to data.lmdb")]
+    #[structopt(short = "p", name = "Path to LMDB data file.")]
     lmdb_path: PathBuf,
+
+    #[structopt(short = "d", name = r#"Database name.
+
+            For storage.lmdb, it should be one of:
+                block_header
+                block_metadata
+                deploys
+                deploy_metadata
+                transfer
+                state_store
+                block_body
+
+            For data.lmdb, it should be TRIE_STORE
+
+    "#)]
+    db_name: String,
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -17,19 +33,24 @@ fn main() -> Result<(), anyhow::Error> {
         .set_max_dbs(1)
         .open(&opts.lmdb_path)?;
 
-    let db = env.create_db(Some("TRIE_STORE"), DatabaseFlags::empty())?;
+    let db = env.create_db(Some(&opts.db_name), DatabaseFlags::empty())?;
+    println!("Scanning LMDB data file: {:?} database name: {}", opts.lmdb_path, opts.db_name);
 
     let txn = env.begin_ro_txn()?;
     let mut cursor = txn.open_ro_cursor(db)?;
-    let mut largest_trie = 0;
+    let mut record_count = 0;
+    let mut largest_record = 0;
 
     for (_key, value) in cursor.iter() {
+        record_count += 1;
         let serialized_len = value.len();
-        if largest_trie < serialized_len {
-            println!("found new largest trie with len {}", serialized_len);
-            largest_trie = serialized_len;
+        if largest_record < serialized_len {
+            println!("found new largest DB entry with len {}", serialized_len);
+            largest_record = serialized_len;
         }
     }
+    println!("processed {} db records total", record_count);
+
 
     Ok(())
 }
