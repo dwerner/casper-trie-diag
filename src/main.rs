b4e942b9d99f8d971dc2d65834ee3e4ef88a1e8c
Daniel Werner <dan@casperlabs.io>
2021-11-03 10:06:22 -0700

    decode into json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
+use casper_execution_engine::storage::trie::Trie;
+use casper_hashing::Digest;
+use casper_types::{bytesrepr::FromBytes, Key, StoredValue};
 use lmdb::{Cursor, DatabaseFlags, Environment, EnvironmentFlags, Transaction};
 use structopt::StructOpt;
 
@@ -8,7 +11,9 @@ struct Opts {
     #[structopt(short = "p", name = "Path to LMDB data file.")]
     lmdb_path: PathBuf,
 
-    #[structopt(short = "d", name = r#"Database name.
+    #[structopt(
+        short = "d",
+        name = r#"Database name.
 
             For storage.lmdb, it should be one of:
                 block_header
@@ -21,7 +26,8 @@ struct Opts {
 
             For data.lmdb, it should be TRIE_STORE
 
-    "#)]
+    "#
+    )]
     db_name: String,
 }
 
@@ -34,14 +40,23 @@ fn main() -> Result<(), anyhow::Error> {
         .open(&opts.lmdb_path)?;
 
     let db = env.create_db(Some(&opts.db_name), DatabaseFlags::empty())?;
-    println!("Scanning LMDB data file: {:?} database name: {}", opts.lmdb_path, opts.db_name);
+    println!(
+        "Scanning LMDB data file: {:?}\ndatabase name: {}",
+        opts.lmdb_path, opts.db_name
+    );
 
     let txn = env.begin_ro_txn()?;
     let mut cursor = txn.open_ro_cursor(db)?;
     let mut record_count = 0;
     let mut largest_record = 0;
 
-    for (_key, value) in cursor.iter() {
+    let mut trie = HashMap::new();
+    for (key, value) in cursor.iter() {
+        if opts.db_name == "TRIE_STORE" {
+            let (key, _rest) = Digest::from_bytes(key)?;
+            let (value, _remainder) = Trie::<Key, StoredValue>::from_bytes(value)?;
+            trie.insert(key, value);
+        }
         record_count += 1;
         let serialized_len = value.len();
         if largest_record < serialized_len {
@@ -49,8 +64,9 @@ fn main() -> Result<(), anyhow::Error> {
             largest_record = serialized_len;
         }
     }
+    let json = serde_json::to_string_pretty(&trie)?;
+    println!("{}", json);
     println!("processed {} db records total", record_count);
-
 
     Ok(())
 }
