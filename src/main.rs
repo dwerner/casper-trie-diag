679229e0bc4fcceece2998c67def4a1c759dba35
Daniel Werner <dan@casperlabs.io>
2021-11-09 08:14:13 -0800

    deserialize and inspecting auction data

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::{collections::HashMap, path::PathBuf};
 
 use casper_execution_engine::storage::trie::Trie;
 use casper_hashing::Digest;
-use casper_types::{bytesrepr::FromBytes, Key, StoredValue};
+use casper_types::{
+    bytesrepr::FromBytes, system::auction::SeigniorageRecipientsSnapshot, Key, StoredValue,
+};
 use lmdb::{Cursor, DatabaseFlags, Environment, EnvironmentFlags, Transaction};
 use structopt::StructOpt;
 
@@ -50,12 +52,68 @@ fn main() -> Result<(), anyhow::Error> {
     let mut record_count = 0;
     let mut largest_record = 0;
 
-    let mut trie = HashMap::new();
     for (key, value) in cursor.iter() {
         if opts.db_name == "TRIE_STORE" {
             let (key, _rest) = Digest::from_bytes(key)?;
-            let (value, _remainder) = Trie::<Key, StoredValue>::from_bytes(value)?;
-            trie.insert(key, value);
+            let (trie, _remainder) = Trie::<Key, StoredValue>::from_bytes(value)?;
+            if let Trie::Leaf {
+                key: trie_key,
+                value: trie_value,
+            } = trie
+            {
+                if let StoredValue::CLValue(cl_value) = trie_value {
+                    let (_, bytes) = cl_value.destructure();
+                    let bytes = bytes.inner_bytes();
+                    let (snapshot, leftover) =
+                        match SeigniorageRecipientsSnapshot::from_bytes(bytes) {
+                            Ok(snapshot) => snapshot,
+                            Err(_) => continue,
+                        };
+                    println!(
+                        "found seignorage snapshot at key {} (value {} bytes)",
+                        trie_key,
+                        bytes.len(),
+                    );
+                    println!(
+                        "{} eras in snapshot: {:?}",
+                        snapshot.keys().len(),
+                        snapshot.keys(),
+                    );
+                    println!(
+                        "{} recipients in all snapshots",
+                        snapshot
+                            .values()
+                            .map(|recipients| recipients.len())
+                            .sum::<usize>()
+                    );
+
+                    println!(
+                        "{} delegator stakes all snapshots",
+                        snapshot
+                            .values()
+                            .map(|recipients| recipients
+                                .values()
+                                .map(|recipient| recipient.delegator_stake().len())
+                                .sum::<usize>())
+                            .sum::<usize>()
+                    );
+
+                    /*
+                    for (era, recipients) in snapshot {
+                        println!("era {} with {} recipients", era, recipients.len());
+                        for (public_key, recipient) in recipients {
+                            println!(
+                                "public key {} recipient delegation rate:{: >8} stake:{: >32} delegators:{: >8}",
+                                public_key,
+                                recipient.delegation_rate(),
+                                recipient.stake(),
+                                recipient.delegator_stake().len(),
+                            );
+                        }
+                    }
+                    */
+                }
+            }
         }
         record_count += 1;
         let serialized_len = value.len();
@@ -64,8 +122,6 @@ fn main() -> Result<(), anyhow::Error> {
             largest_record = serialized_len;
         }
     }
-    let json = serde_json::to_string_pretty(&trie)?;
-    println!("{}", json);
     println!("processed {} db records total", record_count);
 
     Ok(())
