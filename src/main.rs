e8bec265498d14c204ae09e9ae5131912254c255
Daniel Werner <dan@casperlabs.io>
2021-11-30 09:36:59 -0800

    trie analysis

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,6 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opts {
-    #[structopt(short = "n", name = "server to ask for historical auction data")]
-    address: String,
-
     #[structopt(short = "p", name = "Path to LMDB data file.")]
     lmdb_path: PathBuf,
 
@@ -62,87 +59,30 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut record_count = 0;
     let mut largest_record = 0;
 
+    let mut key_tags = HashMap::<String, usize>::new();
+    let mut stored_value_tags = HashMap::<String, usize>::new();
+    let mut trie_lengths = HashMap::<(String, String), Vec<usize>>::new();
+
     for (key, value) in cursor.iter() {
         if opts.db_name == "TRIE_STORE" {
-            let (key, _rest) = Digest::from_bytes(key)?;
+            let (_key, _rest) = Digest::from_bytes(key)?;
+            let byte_len = value.len();
             let (trie, _remainder) = Trie::<Key, StoredValue>::from_bytes(value)?;
             if let Trie::Leaf {
                 key: trie_key,
                 value: trie_value,
             } = trie
             {
-                if let StoredValue::CLValue(cl_value) = trie_value {
-                    if let Err(_) = print_auction_details(None, cl_value) {
-                        continue;
-                    }
+                let key_tag = trie_key.type_string();
+                let stored_value_tag = trie_value.type_name();
 
-                    let auction_key_str = trie_key.to_formatted_string();
-                    println!("found auction state, will traverse blocks and get historical data using this URef {}", auction_key_str);
-
-                    let client = retrieve_state::Client::new();
-                    let url = format!("{}/rpc", opts.address);
-
-                    let highest_block = retrieve_state::get_block(&client, &url, None)
-                        .await?
-                        .block
-                        .expect("no highest block");
-                    println!(
-                        "fetched highest block at height: {}",
-                        highest_block.header.height
-                    );
-
-                    println!("bytes, eras, total_recipients, total_delegator_stakes");
-                    let mut last_auction_size = 0;
-
-                    // we just want a general sense of auction growth, so step by 100 blocks at a time
-                    for height in (1..highest_block.header.height)
-                        .step_by(100)
-                        // but also include the highest block
-                        .chain([highest_block.header.height])
-                    {
-                        let block = retrieve_state::get_block(
-                            &client,
-                            &url,
-                            Some(GetBlockParams {
-                                block_identifier: BlockIdentifier::Height(height as u64),
-                            }),
-                        )
-                        .await?
-                        .block
-                        .unwrap();
-                        let state_root_hash = block.header.state_root_hash;
-                        let auction_state = retrieve_state::get_item(
-                            &client,
-                            &url,
-                            GetItemParams {
-                                state_root_hash,
-                                key: auction_key_str.clone(),
-                                path: Default::default(),
-                            },
-                        )
-                        .await?
-                        .stored_value;
-
-                        if let casper_node::types::json_compatibility::StoredValue::CLValue(
-                            cl_value,
-                        ) = auction_state
-                        {
-                            let auction_size = cl_value.inner_bytes().len();
-                            if auction_size != last_auction_size {
-                                last_auction_size = auction_size;
-                                if let Err(err) =
-                                    print_auction_details(Some(height as usize), cl_value)
-                                {
-                                    println!(
-                                        "error printing auction details at height {} state_root_hash {} {:?}",
-                                        height, state_root_hash, err
-                                    );
-                                    continue;
-                                }
-                            }
-                        }
-                    }
-                }
+                *key_tags.entry(key_tag.clone()).or_default() += 1;
+                *stored_value_tags
+                    .entry(stored_value_tag.clone())
+                    .or_default() += 1;
+                let trie_length_values =
+                    trie_lengths.entry((key_tag, stored_value_tag)).or_default();
+                trie_length_values.push(byte_len);
             }
         }
         record_count += 1;
@@ -152,6 +92,30 @@ async fn main() -> Result<(), anyhow::Error> {
             largest_record = serialized_len;
         }
     }
+
+    println!("key_tag, count");
+    for (key_tag, count) in key_tags {
+        println!("\"{}\", {}", key_tag, count);
+    }
+
+    println!("stored_value_tag, count");
+    for (stored_value_tag, count) in stored_value_tags {
+        println!("\"{}\", {}", stored_value_tag, count);
+    }
+
+    println!("key_tag, stored_value_tag, average_len, max_len");
+    for ((key_tag, stored_value_tag), lengths) in trie_lengths {
+        if lengths.is_empty() {
+            continue;
+        }
+        let average_len: usize = lengths.iter().sum::<usize>() / lengths.len();
+        let max_len: usize = *lengths.iter().max().unwrap();
+        println!(
+            "\"{}\", \"{}\", {}, {}",
+            key_tag, stored_value_tag, average_len, max_len
+        );
+    }
+
     println!("processed {} db records total", record_count);
 
     Ok(())
